f67a895a5a5a9ffd32eb6aa44aef21925e0f2e00
DeAndre Johnson <dre@phreshr.com>
2019-12-05 02:12:46 +0800

    update

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet");
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.orignalUrl}`);
+
+  next();
+}
+
+function gateKeeper(req, res, next) {
+  const { password } = req.header;
+  console.log(password);
+
+  next();
+}
+
 server.use(express.json());
+// server.use(helmet());
+server.use(logger);
 
-server.use('/api/hubs', hubsRouter);
+server.use("/api/hubs", hubsRouter);
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
+});
+
+server.get("/echo", (req, res) => {
+  res.send(req.header);
+});
+
+server.get("/area51", gateKeeper(), (req, res) => {
+  res.send(req.header);
 });
 
 module.exports = server;
